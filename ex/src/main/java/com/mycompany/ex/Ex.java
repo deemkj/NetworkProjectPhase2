6eea8b16fb14934aa57b4998701d4db0dd70eded
lameessa <444201177@student.ksu.edu.sa>
2024-11-22 21:14:02 +0300

    example

---
+++ ex/src/main/java/com/mycompany/ex/Ex.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex;
+
+/**
+ *
+ * @author lamee
+ */
+public class Ex {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
